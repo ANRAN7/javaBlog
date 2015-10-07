dc158d1eb72627a6996c8dc63407089f69f3ea60
wpeace1212 <wpeace1212@gmail.com>
2015-10-07 16:22:21 +0800

    Two

--- test/src/net/peace/Test.java
+++
@@ -1,9 +0,0 @@
-package net.peace;
-import java.util.*;
-public class Test {
-public static void main(String[] args){
-	String a="peacej";
-	System.out.println("hello,world");
-	System.out.println(a+a.length()+a.charAt(2));
-}
-}
